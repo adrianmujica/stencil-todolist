76dd93db293a04a38654ed75fb75aa98cbc691e6
Adrián <adrian.mujicagonzalez.ext@telefonicaed.com>
2018-01-19 13:59:16 +0100

    added control functionalities and style

--- src/components/todo-list/todo-list.tsx
+++ src/components/todo-list/todo-list.tsx
@@ -28,9 +28,10 @@ export class MyComponent {
     // item functions
     addTodo() {
         let item: TodoItemModel = {
-            text: `todo-item: ${this.itemList.length}`, priority: "low", selected: false, id: this.itemList.length
+            text: "", priority: "low", selected: false, id: this.itemList.length
         }
         this.itemList = [...this.itemList, item];
+        this.addItemListEmpty(item.id);
     }
     addItemListEmpty(id: number) {
         this.itemListEmpty = [...this.itemListEmpty, id];
@@ -41,6 +42,36 @@ export class MyComponent {
             .filter((item) => item.id !== id)
             .map((result, index) => result = {... result, id: index})
         ];
+        this.updateEmptyList(id);
+    }
+    updateEmptyList(id: number){
+        this.itemListEmpty = [...this.itemListEmpty.map((i) => id < i ? --i : i)];
+    }
+
+    handleInput(ev, id: number){
+        let index = this.itemList.findIndex((item) => item.id === id);
+        this.itemList[index].text = ev.target.value;
+        this.itemList = [... this.itemList];
+        if (ev.target.value === ""){
+            this.addItemListEmpty(id);
+        } else{
+            this.removeItemListEmpty(id);
+        }
+    }
+    removeItemListEmpty(id: number){
+        this.itemListEmpty = [...this.itemListEmpty.filter((i) => i != id)];
+    }
+
+    changePriority(ev, id: number){
+        let index = this.itemList.findIndex((item) => item.id === id);
+        this.itemList[index].priority = ev.target.value;
+        this.itemList = [... this.itemList];
+    }
+
+    lockTodo(ev, id: number){
+        let index = this.itemList.findIndex((item) => item.id === id);
+        this.itemList[index].selected = ev.target.checked;
+        this.itemList = [... this.itemList];
     }
 
 
@@ -50,8 +81,35 @@ export class MyComponent {
             todos = this.itemList.map(
                 (todo) =>
                     <div class={todo.priority}>
-                        <label>{todo.text}</label>
-                        <label onClick={() => this.removeTodo(todo.id)} >&#10005;</label>
+
+                        <input type="text" 
+                            value={todo.text} 
+                            disabled={todo.selected} 
+                            style={{ textDecoration: todo.selected ? 'line-through' : 'none' }}
+                            onInput={(ev) => this.handleInput(ev, todo.id)}                        
+                        />
+
+                        <button 
+                            disabled={(todo.text.trim().length === 0)} 
+                            onClick={() => this.removeTodo(todo.id)} >
+                            &#10005;
+                        </button>
+                        
+                        <select disabled={todo.selected} 
+                            onChange={(ev) => this.changePriority(ev, todo.id)}>
+                            <option disabled>Priority</option>
+                            <option value="low" selected={todo.priority === "low"} >low</option>
+                            <option value="middle" selected={todo.priority === "middle"} >middle</option>
+                            <option value="high" selected={todo.priority === "high"}>high</option>
+                        </select> 
+
+                        <input 
+                            type="checkbox" 
+                            checked={todo.selected} 
+                            disabled={todo.text.trim().length === 0}                        
+                            onChange={(ev) => this.lockTodo(ev, todo.id)} 
+                        />
+
                     </div>
             )
         }
@@ -59,6 +117,8 @@ export class MyComponent {
             <div>
                 {todos}
                 <button
+                    id="addBtn"
+                    disabled={this.itemListEmpty.length > 0}
                     onClick={() => this.addTodo()}>
                     ADD
                 </button>
